33a7284376917d12011bc45a929415517c24fd26
Denis Grischenko <grischenkodenis@gmail.com>
2021-07-24 12:23:00 +0300

    adds ts-jest

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,7 +4,7 @@ import s from "./style.module.css"
 export default function App(): ReactElement {
 	return (
 		<div>
-			<h1 className={s.container}>Test task react app 2</h1>
+			<h1 className={s.container}>Test task react app</h1>
 		</div>
 	)
 }
